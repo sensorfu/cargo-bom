08bc57f1d11a617c19c3165a79a9cd34df555e37
Ossi Herrala <oherrala@iki.fi>
2024-09-27 14:41:19 +0300

    Replace cargo crate with cargo_metadata

--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,82 @@
 use std::collections::BTreeSet;
 use std::fmt;
-use std::io;
-use std::io::prelude::*;
-use std::path;
-use std::str;
+use std::io::{self, Write};
+use std::path::PathBuf;
 
-use anyhow::Result;
-use cargo::core::dependency::DepKind;
-use cargo::core::package::PackageSet;
-use cargo::core::{Package, Resolve, Workspace};
-use cargo::ops;
-use cargo::util::Config;
-use cargo::CargoResult;
-use structopt::StructOpt;
+use cargo_metadata::{camino, DependencyKind};
+use itertools::Itertools;
 use tabled::Tabled;
 
-#[derive(StructOpt)]
-#[structopt(bin_name = "cargo")]
-enum Opts {
-    #[structopt(name = "bom")]
-    /// Display a Bill-of-Materials for Rust project
-    Bom(Args),
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    bom: Option<BomCli>,
 }
 
-#[derive(StructOpt)]
-struct Args {
-    /// List all dependencies instead of only top level ones
-    #[structopt(long = "all", short = "a")]
-    all: bool,
-    /// Directory for all generated artifacts
-    #[structopt(long = "target-dir", value_name = "DIRECTORY", parse(from_os_str))]
-    target_dir: Option<path::PathBuf>,
-    #[structopt(long = "manifest-path", value_name = "PATH", parse(from_os_str))]
-    /// Path to Cargo.toml
-    manifest_path: Option<path::PathBuf>,
-    #[structopt(long = "verbose", short = "v", parse(from_occurrences))]
-    /// Use verbose output (-vv very verbose/build.rs output)
-    verbose: u32,
-    #[structopt(long = "quiet", short = "q")]
-    /// No output printed to stdout other than the tree
-    quiet: bool,
-    #[structopt(long = "color", value_name = "WHEN")]
-    /// Coloring: auto, always, never
-    color: Option<String>,
-    #[structopt(long = "frozen")]
-    /// Require Cargo.lock and cache are up to date
-    frozen: bool,
-    #[structopt(long = "locked")]
-    /// Require Cargo.lock is up to date
-    locked: bool,
-    #[structopt(long = "offline")]
-    /// Run without accessing the network
-    offline: bool,
-    #[structopt(short = "Z", value_name = "FLAG")]
-    /// Unstable (nightly-only) flags to Cargo
-    unstable_flags: Vec<String>,
+#[derive(Debug, Subcommand)]
+enum BomCli {
+    Bom {
+        /// Path to Cargo.toml
+        #[arg(long)]
+        manifest_path: Option<PathBuf>,
+    },
 }
 
-#[derive(Debug, Tabled, PartialEq, Eq, PartialOrd, Ord)]
-struct DepTable {
-    #[tabled(rename = "Name")]
-    name: String,
-    #[tabled(rename = "Version")]
-    version: String,
-    #[tabled(rename = "Licenses")]
-    licenses: String,
-}
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    let mut cmd = cargo_metadata::MetadataCommand::new();
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct LicenseTable {
-    name: String,
-    version: String,
-    license_files: BTreeSet<path::PathBuf>,
-}
+    if let Some(bom) = cli.bom {
+        match bom {
+            BomCli::Bom { manifest_path } => {
+                if let Some(path) = manifest_path {
+                    cmd.manifest_path(path);
+                }
+            }
+        }
+    }
 
-fn main() -> Result<()> {
-    let mut config = Config::default()?;
-    let Opts::Bom(args) = Opts::from_args();
-    real_main(&mut config, args)
-}
-
-fn real_main(config: &mut Config, args: Args) -> Result<()> {
-    config.configure(
-        args.verbose,
-        args.quiet,
-        args.color.as_deref(),
-        args.frozen,
-        args.locked,
-        args.offline,
-        &args.target_dir,
-        &args.unstable_flags,
-        &[],
-    )?;
-
-    let manifest = args
-        .manifest_path
-        .unwrap_or_else(|| config.cwd().join("Cargo.toml"));
-    let ws = Workspace::new(&manifest, config)?;
-    let members: Vec<Package> = ws.members().cloned().collect();
-    let (package_ids, resolve) = ops::resolve_ws(&ws)?;
-
-    let dependencies = if args.all {
-        all_dependencies(&members, package_ids, resolve)?
-    } else {
-        top_level_dependencies(&members, package_ids)?
-    };
+    let metadata = cmd.exec()?;
 
     let mut depencies_list = BTreeSet::new();
     let mut licenses_list = BTreeSet::new();
 
-    for package in &dependencies {
-        let name = package.name().to_string();
-        let version = format!("{}", package.version());
-        let licenses = format!("{}", package_licenses(package));
-        let license_files = package_license_files(package)?;
-        depencies_list.insert(DepTable {
-            name: name.clone(),
-            version: version.clone(),
-            licenses,
-        });
-        licenses_list.insert(LicenseTable {
-            name,
-            version,
-            license_files,
-        });
+    let members = metadata.workspace_packages();
+
+    for member in &members {
+        for dependency in &member.dependencies {
+            // We only care about normal dependencies
+            if dependency.kind != DependencyKind::Normal {
+                continue;
+            }
+
+            if let Some(dep) = metadata.packages.iter().find(|p| p.name == dependency.name) {
+                // Skip crates in repository
+                if members.iter().any(|m| m.name == dep.name) {
+                    continue;
+                }
+
+                let name = dep.name.clone();
+                let version = dep.version.to_string();
+                let licenses = package_licenses(dep).to_string();
+                let license_files = package_license_files(dep)?;
+
+                depencies_list.insert(DepTable {
+                    name: name.clone(),
+                    version: version.clone(),
+                    licenses,
+                });
+
+                licenses_list.insert(LicenseTable {
+                    name,
+                    version,
+                    license_files,
+                });
+            }
+        }
     }
 
     fn make_table(list: BTreeSet<DepTable>) -> String {
@@ -135,8 +88,7 @@ fn real_main(config: &mut Config, args: Args) -> Result<()> {
 
     let table = make_table(depencies_list);
 
-    let stdout = io::stdout();
-    let mut out = stdout.lock();
+    let mut out = io::stdout().lock();
 
     out.write_all(table.as_bytes())?;
     out.write_all(b"\n")?;
@@ -152,7 +104,7 @@ fn real_main(config: &mut Config, args: Args) -> Result<()> {
             continue;
         }
 
-        writeln!(out, "\n-----BEGIN {} {} LICENSES-----", name, version)?;
+        writeln!(out, "\n-----BEGIN {name} {version} LICENSES-----")?;
 
         let mut licenses_to_print = license_files.len();
         for file in license_files {
@@ -164,123 +116,32 @@ fn real_main(config: &mut Config, args: Args) -> Result<()> {
             }
         }
 
-        writeln!(out, "\n-----END {} {} LICENSES-----", name, version)?;
+        writeln!(out, "\n-----END {name} {version} LICENSES-----")?;
         out.flush()?;
     }
 
     Ok(())
 }
 
-fn top_level_dependencies(
-    members: &[Package],
-    package_ids: PackageSet<'_>,
-) -> CargoResult<BTreeSet<Package>> {
-    let mut dependencies = BTreeSet::new();
-
-    for member in members {
-        for dependency in member.dependencies() {
-            // Filter out Build and Development dependencies
-            match dependency.kind() {
-                DepKind::Normal => (),
-                DepKind::Build | DepKind::Development => continue,
-            }
-            if let Some(dep) = package_ids
-                .package_ids()
-                .find(|id| dependency.matches_id(*id))
-            {
-                let package = package_ids.get_one(dep)?;
-                dependencies.insert(package.to_owned());
-            }
-        }
-    }
-
-    // Filter out our own workspace crates from dependency list
-    for member in members {
-        dependencies.remove(member);
-    }
-
-    Ok(dependencies)
-}
-
-fn all_dependencies(
-    members: &[Package],
-    package_ids: PackageSet<'_>,
-    resolve: Resolve,
-) -> CargoResult<BTreeSet<Package>> {
-    let mut dependencies = BTreeSet::new();
-
-    for package_id in resolve.iter() {
-        let package = package_ids.get_one(package_id)?;
-        if members.contains(package) {
-            // Skip listing our own packages in our workspace
-            continue;
-        }
-        dependencies.insert(package.to_owned());
-    }
-
-    Ok(dependencies)
-}
-
-fn package_licenses(package: &Package) -> Licenses<'_> {
-    let metadata = package.manifest().metadata();
-
-    if let Some(ref license_str) = metadata.license {
-        let licenses: BTreeSet<&str> = license_str
-            .split("OR")
-            .flat_map(|s| s.split("AND"))
-            .flat_map(|s| s.split('/'))
-            .map(str::trim)
-            .collect();
-        return Licenses::List(licenses);
-    }
-
-    if let Some(ref license_file) = metadata.license_file {
-        return Licenses::File(license_file);
-    }
-
-    Licenses::Missing
-}
-
 static LICENCE_FILE_NAMES: &[&str] = &["LICENSE", "UNLICENSE", "COPYRIGHT"];
 
-fn package_license_files(package: &Package) -> io::Result<BTreeSet<path::PathBuf>> {
-    let mut result = BTreeSet::new();
-
-    let path = package
-        .manifest_path()
-        .parent()
-        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Package manifest path missing"))?;
-
-    let metadata = package.manifest().metadata();
-    if let Some(ref license_file) = metadata.license_file {
-        let file = path.join(license_file);
-        if file.exists() {
-            result.insert(file);
-        }
-    }
-
-    for entry in path.read_dir()?.flatten() {
-        if let Ok(name) = entry.file_name().into_string() {
-            for license_name in LICENCE_FILE_NAMES {
-                if name.starts_with(license_name) {
-                    result.insert(entry.path());
-                }
-            }
-        }
-    }
-
-    Ok(result)
+#[derive(Debug, Tabled, PartialEq, Eq, PartialOrd, Ord)]
+struct DepTable {
+    #[tabled(rename = "Name")]
+    name: String,
+    #[tabled(rename = "Version")]
+    version: String,
+    #[tabled(rename = "Licenses")]
+    licenses: String,
 }
 
 #[derive(Debug)]
 enum Licenses<'a> {
     // Use BTreeSet to get alphabetical order automatically.
     List(BTreeSet<&'a str>),
-    File(&'a str),
+    File(String),
     Missing,
 }
-
-use itertools::Itertools;
 
 impl<'a> fmt::Display for Licenses<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
@@ -293,4 +154,64 @@ impl<'a> fmt::Display for Licenses<'a> {
             }
         }
     }
+}
+
+fn package_licenses(package: &cargo_metadata::Package) -> Licenses<'_> {
+    if let Some(ref license_str) = package.license {
+        let licenses: BTreeSet<&str> = license_str
+            .split("OR")
+            .flat_map(|s| s.split("AND"))
+            .flat_map(|s| s.split('/'))
+            .map(str::trim)
+            .collect();
+        return Licenses::List(licenses);
+    }
+
+    if let Some(ref license_file) = package.license_file() {
+        return Licenses::File(license_file.to_string());
+    }
+
+    Licenses::Missing
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct LicenseTable {
+    name: String,
+    version: String,
+    license_files: BTreeSet<camino::Utf8PathBuf>,
+}
+
+pub fn package_license_files(
+    package: &cargo_metadata::Package,
+) -> io::Result<BTreeSet<camino::Utf8PathBuf>> {
+    let mut result = BTreeSet::new();
+
+    let path = package
+        .manifest_path
+        .parent()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Package manifest path missing"))?;
+
+    if let Some(ref license_file) = package.license_file() {
+        let file = path.join(license_file);
+        if file.exists() {
+            result.insert(file);
+        }
+    }
+
+    for entry in path.read_dir()?.flatten() {
+        if let Ok(name) = entry.file_name().into_string() {
+            for license_name in LICENCE_FILE_NAMES {
+                if name.starts_with(license_name) {
+                    match camino::Utf8PathBuf::from_path_buf(entry.path()) {
+                        Ok(path) => {
+                            result.insert(path);
+                        }
+                        Err(err) => panic!("Invalid path: {err:?}"),
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(result)
 }
